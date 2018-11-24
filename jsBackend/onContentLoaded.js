987ca3164553d6954f8a7ab119477fa06fe5a206
Daniel <danwhittaker22@gmail.com>
2018-11-24 15:26:26 +0000

    Link gif analysis code to extension

--- jsBackend/onContentLoaded.js
+++ jsBackend/onContentLoaded.js
@@ -1,35 +1,47 @@
 //Can just access document var directly from here
 
-importExternalJs("https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js")
-importExternalJs("https://cdn.rawgit.com/buzzfeed/libgif-js/master/libgif.js")
-
 //alert("Extension is working")
 
-var images = document.images
+$('img').each(function (idx, img_tag)
+{
+if (/^.+\.gif$/.test($(img_tag).prop("src")))
+{
+	var gifFile = new SuperGif({ gif: img_tag, progressbar_height: 0 } );
 
-for (image in images) {
-  // if (typeOf(image.src) != "undefined" && image.src.matches("^.*\.(gif)$"))
-  //   placeholder(image, onGifAnalysed)
-  placeholder(document, onGifAnalysed)
+	gifFile.load(function()
+	{
+		for (var i = 0; i < gifFile.get_length(); i++)
+		{
+		   gifFile.move_to(i);
+
+		   var c = gifFile.get_canvas();
+		   var ctx=c.getContext("2d");
+		   var pixels=ctx.getImageData(0,0,c.width,c.height);
+
+		   console.log("AVG FOR FRAME " + i);
+		   test(pixels);
+		}
+  });
 }
+});
 
 
-//Potentially scan through all images, check if an instance of gif and pass
-//to another file for analysis
 
-function onGifAnalysed(gif, shouldBlockGif) {
-  //images.item(gifNum).
-  alert("gif analysed and is " + (shouldBlockGif ? "blocked" : "good to go"))
-  console.log("gif analysed and is " + (shouldBlockGif ? "blocked" : "good to go"))
-}
+function test(imgData)
+{
+
+	var greySum = 0;
+	for (var i=0;i<imgData.data.length;i+=4)
+	{
+	  var r = imgData.data[i];
+	  var g = imgData.data[i+1];
+	  var b = imgData.data[i+2];
+
+	  greySum += (r+g+b)/3;
+	}
+
+	console.log(greySum/(imgData.data.length / 4));
 
 
-function importExternalJs(src) {
-  var imported = document.createElement("script")
-  imported.src = src
-  document.head.appendChild(imported)
-}
 
-function placeholder(doc, callback) {
-  //callback(gif, true)
 }
