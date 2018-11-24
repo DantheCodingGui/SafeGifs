556c6a6fefdb7e6e7281513d5a560cb9677bb761
Daniel <danwhittaker22@gmail.com>
2018-11-24 13:58:35 +0000

    Add files for basic chrome extension functionality

---
+++ jsBackend/onContentLoaded.js
@@ -0,0 +1,18 @@
+//Can just access document var directly from here
+
+alert("Extension is working")
+
+var images = document.images
+var gifs = []
+
+for (image in images) {
+    
+}
+
+
+//Potentially scan through all images, check if an instance of gif and pass
+//to another file for analysis
+
+function onGifAnalysed(shouldBlockGif, gifNum) {
+  //images.item(gifNum).
+}
