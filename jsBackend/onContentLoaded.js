9ac465d0cedcd716682e896e874b23b5d9ca1011
Daniel <danwhittaker22@gmail.com>
2018-11-24 18:46:47 +0000

    Refactor gif loading code

--- jsBackend/onContentLoaded.js
+++ jsBackend/onContentLoaded.js
@@ -1,71 +1,53 @@
 //Can just access document var directly from here
 
 //alert("Extension is working")
-
-var shouldBlock;
-
-$('img').each(function (idx, img_tag)
-{
-if (/^.+\.(?:G|g)(?:I|i)(?:F|f)$/.test($(img_tag).prop("src")))
-{
-	loadGIF($(img_tag).attr("src"))
-
-
-
-	shouldBlock = false;
-
-  if (shouldBlock)
-    $(img_tag)[0].remove()
-}
-});
-
-
-
-
-
-
-
-
 class Gif_Frame
 {
 	constructor(avgInten, delay){
 		this.avgInten = avgInten;
 		this.delay = delay;
 	}
+}
 
+
+$('img').each(function (idx, img_tag)
+{
+if (/^.+\.(?:G|g)(?:I|i)(?:F|f)$/.test($(img_tag).prop("src")))
+{
+	processGif($(img_tag).attr("src"), img_tag, onAnalysisComplete)
+}
+});
+
+function onAnalysisComplete(tag, shouldBlock) {
+	if (shouldBlock)
+		$(tag)[0].remove()
 }
 
 
 
-// user canvas
-// gif patch canvas
-var tempCanvas = document.createElement('canvas');
-var tempCtx = tempCanvas.getContext('2d');
-// full gif canvas
-var gifCanvas = document.createElement('canvas');
-var gifCtx = gifCanvas.getContext('2d');
 
-loadGIF();
-var gif;
-var counter = 0;
-// load a gif with the current input url value
-function loadGIF(url)
+
+
+
+
+//Grab frames from the gif and pass them on to be tested
+function processGif(url, imageTag, callback)
 {
-
-	console.log(counter);
-	counter++;
-
+	var gif;
 	var oReq = new XMLHttpRequest();
 	oReq.open("GET", url, true);
 	oReq.responseType = "arraybuffer";
 
-	oReq.onload = function (oEvent) {
-	    var arrayBuffer = oReq.response; // Note: not oReq.responseText
-	    if (arrayBuffer) {
+	oReq.onload = function (oEvent) 
+	{
+	    var arrayBuffer = oReq.response;
+	    if (arrayBuffer) 
+		{
 	        gif = new GIF(arrayBuffer);
 	        var frames = gif.decompressFrames(true);
 
-			processGif(frames);
+			//Once frames have been received, it is time to examine them
+			callback(imageTag, examineFrames(frames));
 	    }
 	};
 	oReq.send(null);
@@ -73,65 +55,56 @@ function loadGIF(url)
 
 
 
-
-
-function processGif(frames)
+function examineFrames(frames)
 {
-	//Loop through all frames, for each frame calculate the avg intensity and delay
-
+	//Loop through all frames in the gif and create a Gif_Frame for each consisting of the frames avg intensity and delay time
 	var gifFrames = new Array();
-
 	for (var i = 0; i < frames.length; ++i)
 	{
-
 		var frame = frames[i];
 
-
 		//Get avg intensity
-		var avgInten = calcAvgInten(getImgData(frame));
+		var avgI = calcAvgInten(getImgData(frame));
 		//Add this frame to our frames array
-		//var gf = new Gif_Frame(avgInten, frame.delay);
-	//	console.log("Frame Delay: " + gf.delay + " Frame int: " + gf.avgInten);
-		gifFrames[i] = new Gif_Frame(avgInten, frame.delay);
-
-
+		gifFrames[i] = new Gif_Frame(avgI, frame.delay);
 	}
-
-	var printOut = false;
-
+	
+	
+	
+	
+	
+	
+	var printOut = true;
 	for(var i = 0; i < gifFrames.length; ++i)
 	{
 		if(printOut)
 			console.log("Frame Delay: " + gifFrames[i].delay + " Frame int: " + gifFrames[i].avgInten);
 	}
+	
+		return false;
 
 }
 
+
+//Convert frame data into html canvas image data
 function getImgData(frame)
 {
+	var tempCanvas = document.createElement('canvas');
+	var tempCtx = tempCanvas.getContext('2d');	
 	var frameImageData;
 	var dims = frame.dims;
-
 
 	tempCanvas.width = dims.width;
 	tempCanvas.height = dims.height;
 	frameImageData = tempCtx.createImageData(dims.width, dims.height);
-
-	// set the patch data as an override
 	frameImageData.data.set(frame.patch);
-
-	// draw the patch back over the canvas
-	tempCtx.putImageData(frameImageData, 0, 0);
-
-	gifCtx.drawImage(tempCanvas, dims.left, dims.top);
-
 
 	return frameImageData;
 }
 
+//Calculate the average intensity value over an image defined by an imageData array
 function calcAvgInten(imgData)
 {
-
 	var greySum = 0;
 	for (var i=0;i<imgData.data.length;i+=4)
 	{
@@ -143,6 +116,4 @@ function calcAvgInten(imgData)
 	}
 
 	return greySum/(imgData.data.length / 4);
-
-
 }
